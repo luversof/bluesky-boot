a0f73fa990b8f801770ddac0a60e7769bf756048
bluesky <luversof@hanmail.net>
2017-09-19 00:31:19 +0900

    .

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mybatis/type/UUIDTypeHandler.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mybatis/type/UUIDTypeHandler.java
@@ -8,7 +8,9 @@ import java.util.UUID;
 
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
 
+@MappedJdbcTypes(JdbcType.BINARY)
 public class UUIDTypeHandler extends BaseTypeHandler<UUID> {
 
 	@Override
