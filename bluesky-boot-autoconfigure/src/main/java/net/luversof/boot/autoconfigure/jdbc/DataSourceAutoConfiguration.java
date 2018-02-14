d80e87c23b2325a2f23415ae4a5304e86e06d027
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2018-02-14 16:27:42 +0900

    jdbc 설정 2.0.0.RC1 기준 변경

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfiguration.java
@@ -3,7 +3,11 @@ package net.luversof.boot.autoconfigure.jdbc;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -12,5 +16,19 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @PropertySource("classpath:jdbc/jdbc.properties")
 @PropertySource("classpath:jdbc/jdbc-${spring.profiles.active}.properties")
 public class DataSourceAutoConfiguration {
+
+	
+	@Bean
+	@Primary
+	@ConfigurationProperties("datasource.default")
+	public DataSourceProperties defaultDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+	
+	@Bean
+	@Primary
+	public DataSource defaultDataSource() {
+		return defaultDataSourceProperties().initializeDataSourceBuilder().build();
+	}
 
 }
