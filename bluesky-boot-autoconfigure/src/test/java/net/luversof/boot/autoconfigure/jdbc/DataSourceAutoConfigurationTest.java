70dd196d564f59e6602eb8f206e1d2e21f7e669c
bluesky <luversof@hanmail.net>
2018-08-23 02:54:24 +0900

    .

--- bluesky-boot-autoconfigure/src/test/java/net/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfigurationTest.java
+++ bluesky-boot-autoconfigure/src/test/java/net/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfigurationTest.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DataSourceAutoConfigurationTest {
 	
-	@Value("${datasource.mysql.driver-class-name}")
+	@Value("${datasource.default.url}")
 	String test;
 	
 	
