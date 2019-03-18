eff9da45957c105b44961826923ccd46caa9910b
luver <luver@DESKTOP-VHHSBOB>
2019-03-18 13:01:17 +0900

    default profile 추가

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/constant/ProfileInfo.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/constant/ProfileInfo.java
@@ -6,9 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ProfileInfo {
 
+	public static final String DEFAULT = "default";	// spring cloud config server profile
 	public static final String OP_DEV = "opdev";
 	public static final String LOCAL_DEV = "localdev";
 	public static final String DOCKER_DEV = "dockerdev";
 
-	public static final String[] NET_PROFILES = { OP_DEV, LOCAL_DEV, DOCKER_DEV };
+	public static final String[] NET_PROFILES = { DEFAULT, OP_DEV, LOCAL_DEV, DOCKER_DEV };
 }
