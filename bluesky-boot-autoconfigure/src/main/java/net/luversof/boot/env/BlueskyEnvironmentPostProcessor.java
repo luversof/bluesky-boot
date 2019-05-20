4dc5a3a132cfa651df08ffc7dd7e1920ddfe2615
luversof <luversof@hanmail.net>
2019-05-20 23:38:24 +0900

    메이븐 빌드 시 spring profile설정 문제는 어떻게 하지?

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/env/BlueskyEnvironmentPostProcessor.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/env/BlueskyEnvironmentPostProcessor.java
@@ -8,9 +8,11 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 
+import lombok.extern.slf4j.Slf4j;
 import net.luversof.boot.constant.ProfileInfo;
 import net.luversof.boot.exception.BlueskyException;
 
+@Slf4j
 public class BlueskyEnvironmentPostProcessor implements EnvironmentPostProcessor {
 	
 	private static final String NET_PROFILE = "net-profile";
@@ -19,8 +21,10 @@ public class BlueskyEnvironmentPostProcessor implements EnvironmentPostProcessor
 
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		System.out.println("!@#!@#!@#@!#!@#!@#");
+		log.debug("test :::::::::::::: {}", environment);
 		String[] activeProfiles = environment.getActiveProfiles();
-		
+		log.debug("activeProfiles : {}", Arrays.asList(activeProfiles));
 		String profile = Arrays.stream(activeProfiles).filter(x -> Arrays.stream(ProfileInfo.NET_PROFILES).anyMatch(y -> y.equals(x))).findAny().orElseThrow(() -> new BlueskyException("NOT_EXIST_PROFILE"));
 		
 		properties.setProperty(NET_PROFILE, profile);
