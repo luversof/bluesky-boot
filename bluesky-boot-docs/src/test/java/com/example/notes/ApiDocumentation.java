b90230d656fca78e5e2ce06aac1f38270b442e9d
luversof <luversof@hanmail.net>
2019-10-05 05:54:00 +0900

    spring boot 2.1.9.RELEASE 적용

--- bluesky-boot-docs/src/test/java/com/example/notes/ApiDocumentation.java
+++ bluesky-boot-docs/src/test/java/com/example/notes/ApiDocumentation.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.notes;
 
 import static org.hamcrest.Matchers.is;
@@ -39,29 +23,26 @@ import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringRunner.class)
+@ExtendWith({ SpringExtension.class, RestDocumentationExtension.class })
 @SpringBootTest
 //@ActiveProfiles("local")
 public class ApiDocumentation {
-
-	@Rule
-	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
 
 	@Autowired
 	private NoteRepository noteRepository;
@@ -72,15 +53,14 @@ public class ApiDocumentation {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@Autowired
-	private WebApplicationContext context;
-
 	private MockMvc mockMvc;
 
-	@Before
-	public void setUp() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-				.apply(documentationConfiguration(this.restDocumentation)).build();
+	@BeforeEach
+	public void setUp(WebApplicationContext webApplicationContext,
+			RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.apply(documentationConfiguration(restDocumentation)) 
+				.build();
 	}
 
 	@Test
