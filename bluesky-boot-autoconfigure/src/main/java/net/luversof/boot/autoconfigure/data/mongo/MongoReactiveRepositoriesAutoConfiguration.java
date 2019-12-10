313f353b7a0339c6d0c9b14526f3911ce1c54b61
luversof <luversof@hanmail.net>
2019-12-10 16:06:00 +0900

    primary SimpleReactiveMongoDatabaseFactory 추가
    bluesky-boot-starter-test-reactive module 추가

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/data/mongo/MongoReactiveRepositoriesAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/data/mongo/MongoReactiveRepositoriesAutoConfiguration.java
@@ -1,9 +1,16 @@
 package net.luversof.boot.autoconfigure.data.mongo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.mongodb.reactivestreams.client.MongoClient;
@@ -15,4 +22,15 @@ import com.mongodb.reactivestreams.client.MongoClient;
 @EnableMongoAuditing
 public class MongoReactiveRepositoriesAutoConfiguration {
 
+	@Primary
+	@Bean
+	public SimpleReactiveMongoDatabaseFactory configReactiveMongoDatabaseFactory(MongoClient mongoClient) {
+		return new SimpleReactiveMongoDatabaseFactory(mongoClient, "config");
+	}
+	
+	@Primary
+	@Bean
+	public ReactiveMongoTemplate configReactiveMongoTemplate(@Qualifier("configReactiveMongoDatabaseFactory") ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory, MongoConverter converter) {
+		return new ReactiveMongoTemplate(reactiveMongoDatabaseFactory, converter);
+	}
 }
