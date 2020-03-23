57bf7a067c2f4b9eb28fd726e15ccef467e70f95
luversof <luversof@hanmail.net>
2020-03-23 20:19:22 +0900

    k8sdev profile ProfileInfo 추가

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/constant/ProfileInfo.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/constant/ProfileInfo.java
@@ -10,6 +10,7 @@ public final class ProfileInfo {
 	public static final String OP_DEV = "opdev";
 	public static final String LOCAL_DEV = "localdev";
 	public static final String DOCKER_DEV = "dockerdev";
+	public static final String K8S_DEV = "k8sdev";
 
-	public static final String[] NET_PROFILES = { DEFAULT, OP_DEV, LOCAL_DEV, DOCKER_DEV };
+	public static final String[] NET_PROFILES = { DEFAULT, OP_DEV, LOCAL_DEV, DOCKER_DEV, K8S_DEV };
 }
