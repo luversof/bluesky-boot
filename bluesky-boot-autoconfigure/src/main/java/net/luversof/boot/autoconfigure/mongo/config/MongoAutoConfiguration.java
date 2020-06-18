89f79e488a6f82c5bec447d5a1b8a12555ae67d4
luversof <luversof@hanmail.net>
2020-06-18 23:14:53 +0900

    #1

--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mongo/config/MongoAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mongo/config/MongoAutoConfiguration.java
@@ -3,6 +3,7 @@ package net.luversof.boot.autoconfigure.mongo.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.mongodb.client.MongoClient;
 
@@ -10,6 +11,7 @@ import com.mongodb.client.MongoClient;
 @Configuration(value = "_blueskyMongoAutoConfiguration", proxyBeanMethods = false)
 @ConditionalOnClass(MongoClient.class)
 @EnableConfigurationProperties(MongoProperties.class)
+@PropertySource(value = "classpath:mongo/mongo-${net-profile}.properties", ignoreResourceNotFound = true)
 public class MongoAutoConfiguration {
 
 }
--- bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mongo/config/MongoProperties.java
+++ bluesky-boot-autoconfigure/src/main/java/net/luversof/boot/autoconfigure/mongo/config/MongoProperties.java
@@ -3,11 +3,15 @@ package net.luversof.boot.autoconfigure.mongo.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bson.UuidRepresentation;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
 
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import net.luversof.boot.autoconfigure.mongo.config.MongoProperties.BlueskyMongoProperties.BlueskyMongoPropertiesBuilder;
 
 /**
  * 굳이 module 구성 할 이유가 없어보이는데?
@@ -16,7 +20,7 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @ConfigurationProperties(prefix = "bluesky-modules.mongo")
-public class MongoProperties {
+public class MongoProperties implements InitializingBean {
 
 	private BlueskyMongoProperties defaultProperties;
 	
@@ -24,8 +28,52 @@ public class MongoProperties {
 
 	@Data
 	@EqualsAndHashCode(callSuper = true)
-	@Builder
 	public static class BlueskyMongoProperties extends org.springframework.boot.autoconfigure.mongo.MongoProperties {
+			
+		@Builder
+		public BlueskyMongoProperties(String host, Integer port, String uri, String database,
+				String authenticationDatabase, String gridFsDatabase, String username, char[] password,
+				String replicaSetName, Class<?> fieldNamingStrategy, UuidRepresentation uuidRepresentation,
+				Boolean autoIndexCreation) {
+			setHost(host);
+			setPort(port);
+			setUri(uri);
+			setDatabase(gridFsDatabase);
+			setAuthenticationDatabase(authenticationDatabase);
+			setGridFsDatabase(gridFsDatabase);
+			setUsername(username);
+			setPassword(password);
+			setReplicaSetName(replicaSetName);
+			setFieldNamingStrategy(fieldNamingStrategy);
+			setUuidRepresentation(uuidRepresentation == null ? UuidRepresentation.JAVA_LEGACY : uuidRepresentation);
+			setAutoIndexCreation(autoIndexCreation);
+		}
+	}
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		
+		PropertyMapper propertyMapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
+		
+		for (var key : connectionMap.keySet()) {
+			var blueskyMongoProperties = connectionMap.get(key);
+			
+			BlueskyMongoPropertiesBuilder builder = BlueskyMongoProperties.builder();
+			
+			propertyMapper.from(blueskyMongoProperties::getHost).to(builder::host);
+			propertyMapper.from(blueskyMongoProperties::getPort).to(builder::port);
+			propertyMapper.from(blueskyMongoProperties::getUri).to(builder::uri);
+			propertyMapper.from(blueskyMongoProperties::getGridFsDatabase).to(builder::gridFsDatabase);
+			propertyMapper.from(blueskyMongoProperties::getUsername).to(builder::username);
+			propertyMapper.from(blueskyMongoProperties::getPassword).to(builder::password);
+			propertyMapper.from(blueskyMongoProperties::getReplicaSetName).to(builder::replicaSetName);
+			propertyMapper.from(blueskyMongoProperties::getFieldNamingStrategy).to(builder::fieldNamingStrategy);
+			propertyMapper.from(blueskyMongoProperties::getUuidRepresentation).to(builder::uuidRepresentation);
+			propertyMapper.from(blueskyMongoProperties::isAutoIndexCreation).to(builder::autoIndexCreation);
+			
+			connectionMap.put(key, builder.build());
+			
+		}
+		
 	}
 }
