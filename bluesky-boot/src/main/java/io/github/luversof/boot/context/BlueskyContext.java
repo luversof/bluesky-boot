ed80032452005620d40fba8cec5d9cc37343e98a
bluesky <luversof@hanmail.net>
2021-09-27 17:11:36 +0900

    close #8 BlueskyContext FunctionalInterface로 변경

--- bluesky-boot/src/main/java/io/github/luversof/boot/context/BlueskyContext.java
+++ bluesky-boot/src/main/java/io/github/luversof/boot/context/BlueskyContext.java
@@ -1,22 +1,8 @@
 package io.github.luversof.boot.context;
 
-import io.github.luversof.boot.config.BlueskyCoreModuleProperties;
-import io.github.luversof.boot.config.BlueskyProperties;
-
+@FunctionalInterface
 public interface BlueskyContext {
 	
 	String getModuleName();
-	void setModuleName(String moduleName);
-	
-	/**
-	 * 설정된 moduleName을 기반으로 호출되는 정보
-	 * @return
-	 */
-	<T, U extends BlueskyProperties<T>> T getModule(Class<U> u);
-	
-	/**
-	 * coreModule의 경우 가장 자주 쓰이기 때문에 기본 제공
-	 * @return
-	 */
-	<T extends BlueskyCoreModuleProperties> T getCoreModule();
+
 }
