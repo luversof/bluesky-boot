b6c34f1671e9d1233189b07a42f3b00e83c5e9dd
bluesky <luversof@hanmail.net>
2022-06-12 04:00:46 +0900

    ValidationAutoConfiguration @ConditionalOnClass Advicel.class 추가

--- bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/validation/config/ValidationAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/validation/config/ValidationAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.github.luversof.boot.autoconfigure.validation.config;
 import javax.validation.Validator;
 import javax.validation.executable.ExecutableValidator;
 
+import org.aspectj.weaver.Advice;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import io.github.luversof.boot.autoconfigure.validation.aspect.BlueskyValidatedAspect;
 
 @AutoConfiguration
-@ConditionalOnClass(ExecutableValidator.class)
+@ConditionalOnClass({ ExecutableValidator.class, Advice.class })
 @ConditionalOnResource(resources = "classpath:META-INF/services/javax.validation.spi.ValidationProvider")
 @ConditionalOnProperty(prefix = "spring.aop", name = "auto", havingValue = "true", matchIfMissing = true)
 public class ValidationAutoConfiguration {
