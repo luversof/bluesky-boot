fd4e1bd79e6707fe4b70536066d53300223a62cc
bluesky <luversof@hanmail.net>
2022-06-18 00:00:48 +0900

    .

---
+++ bluesky-boot-docs/src/test/java/com/example/notes/ApiDocumentationTest.java
@@ -0,0 +1,42 @@
+package com.example.notes;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+
+import io.github.luversof.boot.test.autoconfigure.restdocs.RestDocsTest;
+
+@SpringBootTest
+@ActiveProfiles("localdev")
+class ApiDocumentationTest extends RestDocsTest {
+
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp(RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+				.apply(documentationConfiguration(restDocumentation)).build();
+	}
+
+	@Test
+	public void sample() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andDo(MockMvcRestDocumentationWrapper.document("sample"));
+	}
+}
