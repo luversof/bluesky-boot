af4907abc23172c65b22b0dc9ef4bca3d02847a9
bluesky <luversof@hanmail.net>
2022-06-19 01:28:11 +0900

    @BlueskyValidated annotation super class, interface method parameter 까지 체크하도록 개선

--- bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/validation/aspect/BlueskyValidatedAspect.java
+++ bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/validation/aspect/BlueskyValidatedAspect.java
@@ -11,8 +11,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.util.ClassUtils;
 
 import io.github.luversof.boot.autoconfigure.validation.annotation.BlueskyValidated;
+import io.github.luversof.boot.exception.BlueskyException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 
 @Aspect
@@ -30,25 +34,70 @@ public class BlueskyValidatedAspect {
 		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
         
-        var parameters = method.getParameters();
-        var targetIndex = -1;
-        BlueskyValidated targetAnnotation = null;
-        for (int i = 0; i < method.getParameters().length ; i++) {
-        	var annotation = parameters[i].getAnnotation(BlueskyValidated.class);
-        	if (annotation != null) {
-        		targetIndex = i;
-        		targetAnnotation = annotation;
-        		break;
-        	}
-        }
-        var targetObject = joinPoint.getArgs()[targetIndex];
+        var targetClass = joinPoint.getTarget().getClass();
+        MethodParameterInfo methodParameterInfo = getMethofParameterInfoFromAll(targetClass, method.getName(), method.getParameterTypes());
         
-		Set<ConstraintViolation<Object>> result = validator.validate(targetObject, targetAnnotation.value());
+        if (methodParameterInfo == null) {
+        	throw new BlueskyException("can't find method parameter annotation.");
+        }
+        
+        var targetObject = joinPoint.getArgs()[methodParameterInfo.getIndex()];
+        
+		Set<ConstraintViolation<Object>> result = validator.validate(targetObject, methodParameterInfo.getAnnotation().value());
 		
 		if (!result.isEmpty()) {
 			throw new ConstraintViolationException(result);
 		}
 		return joinPoint.proceed();
 	}
+	
+	private MethodParameterInfo getMethofParameterInfoFromAll(Class<?> clazz, String methodName, Class<?>... paramTypes) {
+		var methodParameterInfo = getMethodParameterInfo(clazz, methodName, paramTypes);
+		 
+		if (methodParameterInfo != null) {
+			return methodParameterInfo;
+		}
 
+    	// super class 검색
+    	if (clazz.getSuperclass() != null) {
+    		methodParameterInfo = getMethofParameterInfoFromAll(clazz.getSuperclass(), methodName, paramTypes);
+    	}
+    	
+    	if (methodParameterInfo != null) {
+			return methodParameterInfo;
+		}
+    	
+	    // interface 검색
+	    if (clazz.getInterfaces().length > 0) {
+	    	for (Class<?> iClazz : clazz.getInterfaces()) {
+	    		methodParameterInfo = getMethofParameterInfoFromAll(iClazz, methodName, paramTypes);
+	    		if (methodParameterInfo != null) {
+	    			break;
+	    		}
+	    	}
+	    }
+	    return methodParameterInfo;
+	}
+	
+	private MethodParameterInfo getMethodParameterInfo(Class<?> clazz, String methodName, Class<?>... paramTypes) {
+		var targetMethod = ClassUtils.getMethodIfAvailable(clazz, methodName, paramTypes);
+		if (targetMethod != null) {
+			for (int i = 0; i < targetMethod.getParameters().length;  i++ ) {
+				var annotation = targetMethod.getParameters()[i].getAnnotation(BlueskyValidated.class);
+				if (annotation != null) {
+					return new MethodParameterInfo(i, annotation);
+				}		
+			}
+		}
+		return null;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	public static class MethodParameterInfo {
+		private int index;
+		private BlueskyValidated annotation;
+	}
+
+	// 해당 클래스의 
 }
