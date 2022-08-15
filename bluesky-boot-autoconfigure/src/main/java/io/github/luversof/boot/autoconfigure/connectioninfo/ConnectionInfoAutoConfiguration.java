c9069dfdcf127461ea2aa37d859149cfd9a6e80a
bluesky <luversof@hanmail.net>
2022-08-15 20:53:37 +0900

    ConnectionInfoAutoConfiguration ConditionalOnProperty 조건 추가

--- bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/connectioninfo/ConnectionInfoAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/connectioninfo/ConnectionInfoAutoConfiguration.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ public class ConnectionInfoAutoConfiguration {
 	
 	@Configuration(proxyBeanMethods = false)
 	@ConditionalOnClass({ DataSource.class, JdbcTemplate.class })
+	@ConditionalOnProperty(prefix = "bluesky-modules.connection-info.loaders", name = "datasource-mariadb.enabled", havingValue = "true")
 	public static class DataSourceConnectionInfoMariadbConfiguration {
 		
 		@Bean
