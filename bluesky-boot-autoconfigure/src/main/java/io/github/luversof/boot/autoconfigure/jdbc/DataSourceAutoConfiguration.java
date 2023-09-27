2a0d7e12111a13932842b764102f4e1cbba74ee8
luversof <luversof@hanmail.net>
2023-09-27 12:21:48 +0900

    hibernate query log 변경 적용, DataSourceAutoConfiguration 로드 순서 수정

--- bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfiguration.java
+++ bluesky-boot-autoconfigure/src/main/java/io/github/luversof/boot/autoconfigure/jdbc/DataSourceAutoConfiguration.java
@@ -31,7 +31,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
-@AutoConfiguration(value = "blueskyBootDataSourceAutoConfiguration", after = org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class)
+@AutoConfiguration(value = "blueskyBootDataSourceAutoConfiguration", before = org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class)
 @EnableConfigurationProperties(DataSourceProperties.class)
 @ConditionalOnClass({ DataSource.class, EmbeddedDatabaseType.class })
 @PropertySource(value = "classpath:jdbc/jdbc.properties", ignoreResourceNotFound = true)
