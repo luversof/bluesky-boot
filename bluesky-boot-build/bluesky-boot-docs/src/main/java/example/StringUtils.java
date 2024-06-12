0c944e0bbdbdf0c4403778a8619d3ecd611abb93
luversof <luversof@hanmail.net>
2024-06-12 10:13:43 +0900

    .

---
+++ bluesky-boot-build/bluesky-boot-docs/src/main/java/example/StringUtils.java
@@ -0,0 +1,10 @@
+package example;
+
+public class StringUtils {
+    // tag::contains[]
+    public boolean contains(String haystack, String needle) {
+    	// src/main/java StringUtils
+        return haystack.contains(needle);
+    }
+    // end::contains[]
+}
