cf0dd00f836d625db43b7ff287f98384fe3e5ff4
luversof <luversof@hanmail.net>
2025-01-05 19:41:43 +0900

    connectioninfo test data 설정 수정

---
+++ bluesky-boot-build/bluesky-boot-autoconfigure/src/test/resources/connectioninfo/data_k8sdev.sql
@@ -0,0 +1,11 @@
+INSERT INTO `ConnectionInfo` (`connection`, `url`, `username`, `password`, `extradata`) VALUES
+
+('test1', 'jdbc:mariadb://mariadb-service.opensource:3306/connection_info', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('test2', 'jdbc:mariadb://mariadb-service.opensource:3306/connection_info', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('board', 'jdbc:mariadb://mariadb-service.opensource:3306/board', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('blog', 'jdbc:mariadb://mariadb-service.opensource:3306/blog', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('bookkeeping', 'jdbc:mariadb://mariadb-service.opensource:3306/bookkeeping', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('user', 'jdbc:mariadb://mariadb-service.opensource:3306/user', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null),
+('spring_batch', 'jdbc:mariadb://mariadb-service.opensource:3306/spring_batch', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', '{text}dd2d9a9a3735b9f9a63664dca900b04e34d92759a43d301c74dd60d235c9576c', null)
+
+;
