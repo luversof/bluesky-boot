4c917af7bd6b5d81554d0615116fb0c3c0c1e122
luversof <luversof@hanmail.net>
2025-03-29 23:14:48 +0900

    Add support for bluesky-boot-autoconfigure properties in documentation
    - Update `bluesky-boot-properties.adoc` to include new section
    - Modify `spring-configuration-metadata.js` to handle new module
    - Update `bluesky-boot-properties.html` to include new section in TOC
    - Adjust JavaScript to dynamically load properties for new module
    - Remove unused title field from metadata context
    - Update footer timestamp in HTML file

--- bluesky-boot-build/bluesky-boot-docs/src/main/asciidoc/bluesky-boot/js/spring-configuration-metadata.js
+++ bluesky-boot-build/bluesky-boot-docs/src/main/asciidoc/bluesky-boot/js/spring-configuration-metadata.js
@@ -6,23 +6,23 @@ document.addEventListener("DOMContentLoaded", async function () {
     const metaDataInfo = [
 //        { file: "data/metadata1.json", name: "Metadata 1" },
 //        { file: "data/metadata2.json", name: "Metadata 2" }
-        { moduleName: "bluesky-boot", title: "Bluesky Boot Properties" },
-        { moduleName: "bluesky-boot-autoconfigure", title: "Bluesky Boot Autoconfigure Properties" }
+        { moduleName: "bluesky-boot" },
+        { moduleName: "bluesky-boot-autoconfigure" }
     ]; // 파일 리스트 및 파일 제목 지정
 
-    for (const { moduleName, title, description } of metaDataInfo) {
+    for (const { moduleName, description } of metaDataInfo) {
         try {
             const response = await fetch('properties/' + moduleName + '/spring-configuration-metadata.json');
             const data = await response.json();
             
             // 데이터가 없거나 properties가 없을 경우 기본값 설정
             const context = {
-                title: title, // 상단 제목
                 description: description,
                 properties: data.properties || [] // 속성 리스트
             };
 
             // 템플릿을 사용하여 테이블 생성 후 삽입
+			const container = document.getElementById(moduleName + "-container");
             container.innerHTML += template(context);
         } catch (error) {
             console.error("Error loading JSON:", moduleName, error);
