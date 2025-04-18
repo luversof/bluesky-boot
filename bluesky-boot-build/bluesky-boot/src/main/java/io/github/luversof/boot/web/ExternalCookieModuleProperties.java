d1eeb11f0e2581662f81bb5bddf25a7b8ceaae51
luversof <luversof@hanmail.net>
2025-04-18 16:05:23 +0900

    - Import `@Autowired` annotation from Spring framework - Add `setExternalCookieProperties` method for dependency injection - Set parent property using `ExternalCookieProperties` in the new method - Include a comment explaining the dependency reference order - Ensure compatibility with existing `CookieModuleProperties` structure

--- bluesky-boot-build/bluesky-boot/src/main/java/io/github/luversof/boot/web/ExternalCookieModuleProperties.java
+++ bluesky-boot-build/bluesky-boot/src/main/java/io/github/luversof/boot/web/ExternalCookieModuleProperties.java
@@ -1,5 +1,6 @@
 package io.github.luversof.boot.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import io.github.luversof.boot.context.BlueskyBootContextHolder;
@@ -10,6 +11,14 @@ import io.github.luversof.boot.web.CookieProperties.CookiePropertiesBuilder;
 public class ExternalCookieModuleProperties extends CookieModuleProperties {
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Dependency 참조 순서 처리를 위해 선언
+	 */
+	@Autowired
+	public void setExternalCookieProperties(ExternalCookieProperties externalCookieProperties) {
+		setParent(externalCookieProperties);
+	}
 
 	@Override
 	protected SerializableFunction<String, CookiePropertiesBuilder> getBuilderFunction() {
