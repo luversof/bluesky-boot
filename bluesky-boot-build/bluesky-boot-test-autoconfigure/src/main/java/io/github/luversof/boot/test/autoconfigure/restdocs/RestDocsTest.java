dc5ff45e51ba4229a9e14e68a42e6399132cab25
luversof <luversof@hanmail.net>
2025-12-13 10:59:22 +0900

    Update logging configuration and replace ObjectMapper with JsonMapper in RestDocsTest

--- bluesky-boot-build/bluesky-boot-test-autoconfigure/src/main/java/io/github/luversof/boot/test/autoconfigure/restdocs/RestDocsTest.java
+++ bluesky-boot-build/bluesky-boot-test-autoconfigure/src/main/java/io/github/luversof/boot/test/autoconfigure/restdocs/RestDocsTest.java
@@ -19,19 +19,16 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsTest {
 
 	private static final String DEFAULT_MOCK_RESPONSE_PATH = "src/test/resources/mockResponse/";
 
-	private ObjectMapper objectMapper;
-
 	@Autowired
-	public void setObjectMapper(ObjectMapper objectMapper) {
-		this.objectMapper = objectMapper;
-	}
+	private JsonMapper jsonMapper;
+
 
 	protected MockMvc mockMvc;
 
@@ -59,7 +56,7 @@ public abstract class RestDocsTest {
 
 	protected <T> T getMock(String path, Class<T> valueType) {
 		try {
-			return objectMapper.readValue(getMockString(path), valueType);
+			return jsonMapper.readValue(getMockString(path), valueType);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -68,7 +65,7 @@ public abstract class RestDocsTest {
 	@SuppressWarnings("unchecked")
 	protected <T> List<T> getMockList(String path, Class<T> valueType) {
 		try {
-			return objectMapper.readValue(getMockString(path), List.class);
+			return jsonMapper.readValue(getMockString(path), List.class);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -76,7 +73,7 @@ public abstract class RestDocsTest {
 
 	protected <T> Optional<T> getMockOptional(String path, Class<T> valueType) {
 		try {
-			return Optional.of(objectMapper.readValue(getMockString(path), valueType));
+			return Optional.of(jsonMapper.readValue(getMockString(path), valueType));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
